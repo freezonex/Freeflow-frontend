f497f15d7f997b0f8b09e81e6f9c9f7aa547012b
YangYue128-helen <yangyue1228@hotmail.com>
2024-08-14 17:54:50 +0800

    update supabase iframe

--- src/app/builder/craftBackend/page.js
+++ src/app/builder/craftBackend/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Page() {
   return (
     <iframe
-      src="http://10.10.10.102:8000"
+      src="http://10.10.10.86:8000"
       className="w-full h-full min-h-[95vh]"
     />
   );
