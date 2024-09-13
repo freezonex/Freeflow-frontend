868e09558b9014f65447088415cbebd84e906f70
YangYue128-helen <yangyue1228@hotmail.com>
2024-09-13 10:29:25 +0800

    try get supabase address

--- src/app/builder/craftBackend/page.js
+++ src/app/builder/craftBackend/page.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { headers } from 'next/headers';
 
 function Page() {
+  const header = headers();
+  const ip = (header.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0];
+  console.log('ip', ip);
   return (
     <iframe
-      src="http://127.0.0.1:8000"
+      src={`http://${ip}:8000`}
       className="w-full h-full min-h-[96vh] m-[-2rem] absolute mr-0"
     />
   );
