127ca082f41bf790c2380dc0b0b8108638fa6347
YangYue128-helen <yangyue1228@hotmail.com>
2024-09-13 10:42:00 +0800

    fix address

--- src/app/builder/craftBackend/page.js
+++ src/app/builder/craftBackend/page.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { headers } from 'next/headers';
 
 function Page() {
-  const header = headers();
-  const ip = (header.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0];
-  console.log('ip', ip);
   return (
     <iframe
-      src={`http://${ip}:8000`}
+      src="http://52.77.217.53:8000"
       className="w-full h-full min-h-[96vh] m-[-2rem] absolute mr-0"
     />
   );
