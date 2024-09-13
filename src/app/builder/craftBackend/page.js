a92b18e4533e34462dd9fe409ea73aae1fb26fe9
YangYue128-helen <yangyue1228@hotmail.com>
2024-09-13 09:46:30 +0800

    supabase address

--- src/app/builder/craftBackend/page.js
+++ src/app/builder/craftBackend/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Page() {
   return (
     <iframe
-      src="http://13.212.90.140:8000"
+      src="http://127.0.0.1:8000"
       className="w-full h-full min-h-[96vh] m-[-2rem] absolute mr-0"
     />
   );
